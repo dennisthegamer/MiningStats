9414b2749508f8270d0b80ff627f16b22be8caa7
dennisthegamer <41466710+dennisthegamer@users.noreply.github.com>
2026-07-14 20:40:08 +0200

    fix: guard config screen with isModLoaded (no broken ModMenuApi if YACL absent)

--- fabric/src/main/java/de/dennisthegamer/miningstats/fabric/ModMenuIntegration.java
+++ fabric/src/main/java/de/dennisthegamer/miningstats/fabric/ModMenuIntegration.java
@@ -3,11 +3,18 @@ package de.dennisthegamer.miningstats.fabric;
 import de.dennisthegamer.miningstats.config.ConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        // The config screen needs YACL. If it isn't installed, don't touch ConfigScreen
+        // (which references YACL classes) - returning it would crash ModMenu with a
+        // NoClassDefFoundError ("broken implementation of ModMenuApi").
+        if (!FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")) {
+            return parent -> null;
+        }
         return ConfigScreen::create;
     }
 }
--- neoforge/src/main/java/de/dennisthegamer/miningstats/neoforge/MiningStatsNeoForge.java
+++ neoforge/src/main/java/de/dennisthegamer/miningstats/neoforge/MiningStatsNeoForge.java
@@ -4,6 +4,7 @@ import de.dennisthegamer.miningstats.MiningStatsClient;
 import de.dennisthegamer.miningstats.config.ConfigScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
@@ -14,8 +15,10 @@ public final class MiningStatsNeoForge {
     public MiningStatsNeoForge(ModContainer container) {
         MiningStatsClient.init();
 
-        // Config screen (equivalent of the ModMenu integration on Fabric; needs Cloth Config)
-        container.registerExtensionPoint(IConfigScreenFactory.class,
-                (modContainer, parent) -> ConfigScreen.create(parent));
+        // Only register the config screen when YACL is present (ConfigScreen needs it).
+        if (ModList.get().isLoaded("yet_another_config_lib_v3")) {
+            container.registerExtensionPoint(IConfigScreenFactory.class,
+                    (modContainer, parent) -> ConfigScreen.create(parent));
+        }
     }
 }
